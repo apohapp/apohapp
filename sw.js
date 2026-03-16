b772248b2fcee40e9d805ed7dcf58428d94945db
relevapp <custodiomasinfo@gmail.com>
2026-03-16 15:16:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,16 @@
-const CACHE_NAME = 'relevapp-v1';
+const CACHE_NAME = 'relevapp-v2';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './icono-192.png',
   './icono-512.png'
-  // Si tienes un archivo CSS o un JS principal, agrégalos aquí también, ej: './style.css'
 ];
 
-// Instalar el Service Worker y guardar en caché
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Interceptar las peticiones para que funcione offline
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
